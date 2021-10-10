bed6f33bfe7fa709fddaf05df0d9a6d6eb78692d
Alex Choi <91334043+gongbaeg@users.noreply.github.com>
2021-10-10 15:23:36 -0400

    Update OutputController.java

--- src/OutputController.java
+++ src/OutputController.java
@@ -1,34 +1,21 @@
-import javafx.application.Application; 
-import javafx.scene.*; 
-import javafx.scene.control.*; 
-import javafx.scene.layout.*; 
-import javafx.stage.*; 
-import javafx.event.*; 
-import javafx.scene.shape.*; 
-import javafx.scene.paint.*; 
-import javafx.scene.effect.*; 
-import javafx.scene.text.*; 
-import javafx.scene.image.*; 
-import javafx.scene.transform.*;
-import java.io.IOException;
-import javafx.animation.*;
-import javafx.util.*;  
-import javafx.fxml.*; 
-
-import javafx.fxml.FXML; 
-import javafx.scene.control.Label; 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class OutputController {
-    @FXML 
+    @FXML
     Label nameLabel1;
-    Label nameLabel2; 
-    Label nameLabel3; 
-    Label nameLabel4; 
-    
-    public void displayCalories(String calculation){
-        nameLabel1.setText(calculation); 
-        nameLabel2.setText(Integer.toString(Integer.parseInt(calculation) - 100)); 
-        nameLabel3.setText(Integer.toString(Integer.parseInt(calculation) - 250));
-        nameLabel4.setText(Integer.toString(Integer.parseInt(calculation) - 500));
+    @FXML
+    Label nameLabel2;
+    @FXML
+    Label nameLabel3;
+    @FXML
+    Label nameLabel4;
+
+    public void displayCalories(int calculation){
+        nameLabel1.setText(Integer.toString(calculation));
+        nameLabel2.setText(Integer.toString(calculation - 100));
+        nameLabel3.setText(Integer.toString(calculation - 250));
+        nameLabel4.setText(Integer.toString(calculation - 500));
     }
+
 }
